52495578083c4e6c967c496743987bf9a3aca832
astromariia <mariia.shevchenko13@gmail.com>
2025-03-28 15:39:34 -0500

    added alu control from single

--- lab3/hdl/riscv_pipelined.sv
+++ lab3/hdl/riscv_pipelined.sv
@@ -284,12 +284,19 @@ module aludec(input  logic       opb5,
        default: case(funct3) // R-type or I-type ALU
                   3'b000:  if (RtypeSub) 
                     ALUControl = 3'b001; // sub
-                  else          
-                    ALUControl = 3'b000; // add, addi
-                  3'b010:    ALUControl = 3'b101; // slt, slti
-                  3'b110:    ALUControl = 3'b011; // or, ori
-                  3'b111:    ALUControl = 3'b010; // and, andi
-                  default:   ALUControl = 3'bxxx; // ???
+                 else
+		                ALUControl = 4'b0000; // add, addi
+		            3'b010: ALUControl = 4'b0101; // slt, slti
+                3'b011: ALUControl = 4'b1001;   // sltu
+		            3'b110: ALUControl = 4'b0011; // or, ori
+		            3'b111: ALUControl = 4'b0010; // and, andi
+                3'b100: ALUControl = 4'b0100; //xor, xori
+                3'b001: ALUControl = 4'b0110; // sll
+                3'b101:
+                  if (funct7b5) ALUControl = 4'b1000; // sra
+                  else ALUControl = 4'b0111; // srl
+
+		  default: ALUControl = 4'bxxx; // ???
 		endcase
      endcase
 endmodule
